852346ae1c28b3aa6a8b75c0d4ec789cee36e831
AkihiroNakamura2020 <72118975+AkihiroNakamura2020@users.noreply.github.com>
2021-02-16 21:19:05 +0900

    Camerafunc

---
+++ Camerafunc/CameraController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private GameObject mainCamera;//メインカメラ格納用
+
+    [SerializeField]
+     GameObject subCamera;
+
+    [SerializeField]
+     GameObject subCamera2;
+
+    [SerializeField]
+    GameObject drone;
+
+    [SerializeField]
+    GameObject drone2;
+
+    public bool onsubc=false;
+    public bool onsubc2 = false;
+
+
+    void Start()
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera");
+        
+        //サブカメラ非アクティブ
+        subCamera.SetActive(false);
+        subCamera2.SetActive(false);
+
+        drone.SetActive(false);
+        drone2.SetActive(false);
+
+    }
+
+    public void ChangeCamera()
+    {
+        if (onsubc2 == false)
+        {
+            drone.SetActive(true);
+
+            mainCamera.SetActive(!mainCamera.activeSelf);
+            subCamera.SetActive(!subCamera.activeSelf);
+            onsubc = !onsubc;
+
+        }
+            
+    }
+    public void ChangeCamera2()
+    {
+        if (onsubc == false)
+        {
+            drone2.SetActive(true);
+
+            mainCamera.SetActive(!mainCamera.activeSelf);
+            subCamera2.SetActive(!subCamera2.activeSelf);
+            onsubc2 = !onsubc2;
+        }
+    }
+
+}
---
+++ Camerafunc/EnvCameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvCameraFollow : MonoBehaviour
+{
+	GameObject player;
+	Vector3 offset;
+
+	void Start()
+	{
+		player = GameObject.FindWithTag("Player");
+	}
+
+	void Update()
+	{
+		Vector3 pos = player.transform.localPosition;
+		offset = new Vector3(0,3,-7);
+
+		transform.localPosition = pos + offset;
+	}
+}
